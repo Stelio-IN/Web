aeb259ab8c122e234f0b30d623d70c5e32a8918a
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2024-01-21 19:03:59 +0200

    add

--- Web/PHP/empresa/contactos.php
+++ Web/PHP/empresa/contactos.php
@@ -1,24 +1,100 @@
+
  <?php 
-//   // echo 'teste';
-//   //verificar se existem dados enviados num formulario
-//
-//   if(empty($_POST)==false){
-//        print_r($_POST);
-//        var_dump($_POST);
-//   }
+ /*
+    //Validacao de formulario
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+    
+        //verificar se existem todos campos
+
+        $erro = '';
+        if(!isset($_POST['text_email']) || !isset($_POST['text_subject']) || !isset($_POST['text_mensagem'])){
+            $erro = 'Pelo menos um dos campos nao existe';
+        }
+        
+          $email = $_POST['text_email'];
+          $subject = $_POST['text_subject'];
+          $mensagem = $_POST['text_mensagem'];
+       //se nao tiver nenhum erro vai tentar validar o email e continuar para o processo seguinte 
+        if(empty($erro)){
+            //verificar se email e valido
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $erro = "Invalid email format";
+            }
+        }
+
+        if(empty($erro)){
+         $resultado =   mail($email,$subject,$mensagem);
+            if($resultado){
+                echo 'sucesso';
+            }else{
+                echo 'erro';
+            }
+        }
+    }
+*/
+?>
+
+<?php
+    // Validacao de formulario
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+        // Verificar se existem todos campos
+        $erro = '';
+        if (!isset($_POST['text_email']) || !isset($_POST['text_subject']) || !isset($_POST['text_mensagem'])) {
+            $erro = 'Pelo menos um dos campos nao existe';
+        }
+        
+        $email = isset($_POST['text_email']) ? htmlspecialchars($_POST['text_email']) : '';
+        $subject = isset($_POST['text_subject']) ? htmlspecialchars($_POST['text_subject']) : '';
+        $mensagem = isset($_POST['text_mensagem']) ? htmlspecialchars($_POST['text_mensagem']) : '';
+
+        // Se não tiver nenhum erro vai tentar validar o email e continuar para o processo seguinte 
+        if (empty($erro)) {
+            // Verificar se o e-mail é válido
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $erro = "Invalid email format";
+            }
+        }
+
+        if (empty($erro)) {
+            // Configurar cabeçalhos adicionais, se necessário
+            $additional_headers = [
+                'From' => 'hoteltransilvania@gmail.com', // Substitua com o endereço de e-mail desejado
+                'Reply-To' => $email,
+                'X-Mailer' => 'PHP/' . phpversion()
+            ];
+
+            // Parâmetros adicionais, se necessário
+            $additional_params = "";
+
+            // Enviar e-mail
+            $resultado = mail($email, $subject, $mensagem, $additional_headers, $additional_params);
+
+            if ($resultado) {
+                echo 'sucesso';
+            } else {
+                echo 'erro';
+            }
+        }
+    }
 ?>
 
 
 <h1>Contactos</h1>
-
-<form action="tratar.php" method="post" name="meu_form" onsubmit="return validar()">
-    <input type="text" name="text_usuario">
-    <input type="password" name="text_senha">
-    <input type="submit" value="Entrar">
+<!-- <form action="tratar.php" method="post" name="meu_form"  onsubmit="return validar()">   -->
+<form action="?p=contactos" method="post">
+  <input type="email" name="text_email"  placeholder="Email" required> <br>
+  <input type="text" name="text_subject" placeholder="Assunto" required><br>
+  <textarea name="text_mensagem"  cols="40" rows="3" required></textarea> <br>
+  <input type="submit" value="Enviar Mensagem">
 </form> 
 
-<div style="color:red; margin-left: 5px;" id="erro"></div>
+<!-- esse codigo so sera executado se a codicao for satisfeita-->
+<?php if(!empty($erro)):?>
+    <div style="color:red;"><?php echo $erro ?></div>
+<?php endif; ?>
 
+<!-- validacao com JS-->
 <script>
     function validar(){
         let text_usuario = document.forms['meu_form']['text_usuario'].value;
--- Web/PHP/empresa/tratar.php
+++ Web/PHP/empresa/tratar.php
@@ -3,10 +3,39 @@
 //    $apelido = 'Mondlane';
 //    echo $nome .' '.$apelido 
     /** array asssociativo cada valor com uma chave de string*/
+
+    //Verificar se os campos existem
+    if(isset($_POST['text_usuario'])==false || isset($_POST['text_senha'])==false){
+        die('Erro - Nao existem os campos necessarios');
+    }
+
+    //Verificar se os campos estao preencchidos
+    if(empty($_POST['text_usuario'])){
+        die('Erro - campo usuario vazio');
+    }else if(empty($_POST['text_senha'])){
+        die('Erro - campo senha vazio');
+    }
+
+    //validar pelo tipo de campo
     $usuario = $_POST['text_usuario'];
     $senha = $_POST['text_senha'];
-    echo $usuario;
-    echo '<br>';
-    echo $senha;
+
+    if(strlen($usuario)<5 ||strlen($usuario)>10){
+        die('Erro - Usario tem que ter entre 5 e 10');
+    }
+    if(strlen($senha)<3 ||strlen($senha)>15){
+        die('Erro - senha tem que ter entre 3 e 15');
+    }
+
+    //verificar se usario e senha existem
+   $u = 'stelio';
+   $s = '123456';
+
+   if($usuario == $u && $senha ==$s ){
+        die('Login efectuado com sucesso');
+   }else{
+        die('login invalido');
+   }
+
 
 ?>    
